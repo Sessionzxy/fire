b6127a925a8f5170c31728c1b35ee15592233a87
zhaoxiaoya <1357846810@qq.com>
2018-12-18 14:36:51 +0800

    added a new footer [issue 53]

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -31,7 +31,7 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1,-
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
